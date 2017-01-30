ab2c65215bb565ad1e78e98896cd4e910bedca5c
timocodex <timothy.og.siahaan@gmail.com>
2017-01-30 16:14:54 +0700

    controller done & routing (release 2&3)

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,10 @@ router.get('/users', con.findAllUsers)
 
 router.get('/users/:id', con.findUser)
 
+router.post('/users', con.createUser)
+
+router.put('/users/:id', con.updateUser)
+
+router.delete('/users/:id',con.deleteUser)
 
 module.exports = router;
