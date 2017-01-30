f79823fa240a2fb489c76802f6e6aeaf7c843689
timocodex <timothy.og.siahaan@gmail.com>
2017-01-30 16:39:01 +0700

    migrasi ke postgre

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
